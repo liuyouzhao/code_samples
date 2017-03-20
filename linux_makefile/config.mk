5453e49295a843e961355319832655034b38d16c
hujia.liuhj <hujia.liuhj@alibaba-inc.com>
2017-03-20 10:29:52 +0800

    [config.mk&Makefile] Modified that let out folder can be configured as a option parameter

--- linux_makefile/config.mk
+++ linux_makefile/config.mk
@@ -1,10 +1,14 @@
 # Prefix platform
 PLATFORM=x86
 BUILD=build
+BOARD       =
+CPU         =
 
 # Makefile main logic
 ROOT=$(shell pwd)
 PRE_ROOT=$(ROOT)
+
+# toolchain gcc prefix
 CC    =gcc
 AS    =as
 LD    =gcc
@@ -12,13 +16,14 @@ OC    =cp
 LDIR  =
 LDIR2 =
 
-OUT_DIR = ./out/$(PLATFORM)
-OUT_BIN = output
-BOARD       =
-CPU         =
 
+# output binary path
+OUT = out
+OUT_DIR = ./$(OUT)/$(PLATFORM)
+OUT_BIN = output
+
+# compiling and linking flags
 C_CFLAGS += -I./ -Os
 AFLAGS=
 CFLAGS= $(C_CFLAGS)
-
 LDFLAGS = -lc -lm 
