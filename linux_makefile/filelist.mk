e7fb2f237bd63fa0fbbdbaddc40aed88bd60ab7b
hujia.liuhj <hujia.liuhj@alibaba-inc.com>
2017-03-20 10:36:15 +0800

    [linux_makefile/filelist.mk] Modified deleted all commented lines in fielist.mk for short

--- linux_makefile/filelist.mk
+++ linux_makefile/filelist.mk
@@ -14,43 +14,6 @@
  # limitations under the License.
 ##
 
-### C files
-#C_UTIL_FILES = utils/debug.c
-
-#C_ARCH_FILES = \
-               arch/$(PLATFORM)/$(BOARD)/init.c \
-               arch/$(PLATFORM)/$(BOARD)/irq.c \
-               arch/$(PLATFORM)/$(BOARD)/fiq.c \
-               arch/$(PLATFORM)/$(BOARD)/timer.c \
-               arch/$(PLATFORM)/$(BOARD)/arch.c \
-               arch/$(PLATFORM)/$(BOARD)/exc.c \
-               arch/$(PLATFORM)/$(BOARD)/swi.c
-
-#C_CORE_FILES = core/rc_mem.c \
-               core/rc_task.c \
-               core/rc_queue.c \
-               core/rc_timer.c
-
-#C_TEST_FILES = test/test_mem.c
-
-#C_FILES += $(C_BOOT_FILES)
-#C_FILES += $(C_CORE_FILES)
-#C_FILES += $(C_UTIL_FILES)
-#C_FILES += $(C_ARCH_FILES)
-#C_FILES += $(C_TEST_FILES)
-
-### ASM files
-#S_BOOT_FILES = arch/$(PLATFORM)/$(BOARD)/startup.s
-
-#S_CORE_FILES = arch/$(PLATFORM)/$(BOARD)/mpu.s \
-               arch/$(PLATFORM)/$(BOARD)/port.s
-
-#S_FILES += $(S_BOOT_FILES)
-#S_FILES += $(S_CORE_FILES)
-
-### linker file
-#LD_LINKER = arch/$(PLATFORM)/$(BOARD)/linker.ld
-
 C_FILES += test.c
 LD_LINER=
 
